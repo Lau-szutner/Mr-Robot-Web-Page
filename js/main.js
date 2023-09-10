f6a6465a3c2cd16832b99fde2cc0683b562e145e
Lautaro Fernandez Szutner <Lautaroszutner@gmail.com>
2023-09-09 22:12:08 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// let personaje = function () {};
+
+// carga = (nombre) => {
+//   console.log(nombre);
+// };
+
+// carga(prompt('sdsf'));
+
+const cajon = ['olla', 'manija', 'nananan'];
+
+const ollas = new Array();
+
+ollas.push('ola');
